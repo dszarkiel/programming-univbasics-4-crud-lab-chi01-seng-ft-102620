cafc05efd33143ff140697d852a1ebf76ad7ab34
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-05 17:08:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["wow", "I", "am", "really", "learning"]
-  array.pop("learning")
+  last_element = array.pop
 end
 
 def remove_element_from_start_of_array(array)
